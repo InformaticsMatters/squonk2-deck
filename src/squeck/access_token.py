0908fec92ebcc28144d624a53611d4a736fcaabd
a.b.christie <alan.christie@matildapeak.com>
2026-03-11 22:52:13 +0000

    fix: Remove Optional & new lock

--- src/squeck/access_token.py
+++ src/squeck/access_token.py
@@ -1,6 +1,5 @@
 """Gets AS and DM access tokens."""
 
-from typing import Optional
 from squonk2.auth import Auth
 from squonk2.environment import Environment
 
@@ -10,15 +9,15 @@ class AccessToken:
 
     @classmethod
     def get_as_access_token(
-        cls, env: Environment, *, prior_token: Optional[str] = None
-    ) -> Optional[str]:
+        cls, env: Environment, *, prior_token: str | None = None
+    ) -> str | None:
         """Returns a token for the AS API.
         This returns None on error or if the client ID is not defined.
         """
         assert env
         if not env.keycloak_as_client_id:
             return None
-        access_token: Optional[str] = Auth.get_access_token(
+        access_token: str | None = Auth.get_access_token(
             keycloak_url=env.keycloak_url,
             keycloak_realm=env.keycloak_realm,
             keycloak_client_id=env.keycloak_as_client_id,
@@ -31,14 +30,14 @@ class AccessToken:
 
     @classmethod
     def get_dm_access_token(
-        cls, env: Environment, *, prior_token: Optional[str] = None
-    ) -> Optional[str]:
+        cls, env: Environment, *, prior_token: str | None = None
+    ) -> str | None:
         """Returns a token for the DM API
         or None if a token could not be obtained."""
         assert env
         if not env.keycloak_dm_client_id:
             return None
-        access_token: Optional[str] = Auth.get_access_token(
+        access_token: str | None = Auth.get_access_token(
             keycloak_url=env.keycloak_url,
             keycloak_realm=env.keycloak_realm,
             keycloak_client_id=env.keycloak_dm_client_id,
